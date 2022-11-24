405ea8430eb6b74653ba850b4712426f4e79a3e1
Gaby Vazquez <gaby27anahi02@outlook.es>
2022-11-24 11:11:13 -0600

    indexjs

---
+++ index.js
@@ -0,0 +1,17 @@
+        // Import the functions you need from the SDKs you need
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+      
+        // Your web app's Firebase configuration
+        const firebaseConfig = {
+          apiKey: "AIzaSyCkknWySXgon_PCQ6FD55LW0X7WaEO5h_k",
+          authDomain: "fir-javascript-crud-c3f0f.firebaseapp.com",
+          projectId: "fir-javascript-crud-c3f0f",
+          storageBucket: "fir-javascript-crud-c3f0f.appspot.com",
+          messagingSenderId: "795484786355",
+          appId: "1:795484786355:web:4c728f2c1ad901506f2ac6"
+        };
+      
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
